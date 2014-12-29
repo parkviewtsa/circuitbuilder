43b9d390f08e3927589900d3796da5891903bb07
Brent Bessemer <bmb0610@gmail.com>
2014-12-29 18:11:33 -0500

    Moved some stuff from Wire's constructor

--- src/BATCAD/src/bc/desc/Wire.java
+++ src/BATCAD/src/bc/desc/Wire.java
@@ -23,31 +23,32 @@ public class Wire {
   float height;
   float width;
 
-  public Wire (Terminal a, Terminal b) {
+  public Wire(Terminal a, Terminal b) {
 	from = a;
 	to = b;
-	width = Math.abs(a.getGlobalXPos() - b.getGlobalXPos());
-	height = Math.abs(a.getGlobalYPos() - b.getGlobalYPos());
-	float pos_x = a.getGlobalXPos();
-	float pos_y = a.getGlobalYPos();
-	
+	draw();
+  }
+
+  public final void draw() {
+	width = Math.abs(from.getGlobalXPos() - to.getGlobalXPos());
+	height = Math.abs(from.getGlobalYPos() - to.getGlobalYPos());
+	float pos_x = Math.min(from.getGlobalXPos(), to.getGlobalXPos());
+	float pos_y = Math.min(from.getGlobalYPos(), to.getGlobalYPos());
+
 	Element g = svg.createElement("g");
 	g.setAttribute("style",
-			"transform:translate(" + pos_x + "," + pos_y + ");" );
+			"transform:translate(" + pos_x + "," + pos_y + ");");
+	
 	Element e = svg.createElement("polyline");
-
 	if (width == 0.0 || height == 0.0) {
 	  /* If the two components we are trying to connect line up either
 	   * horizontally or vertically, it's easy. We simply draw a line from
 	   * one terminal to the other. */
-	 
 	  e.setAttribute("points", "0,0 " + width + "," + height);
 	  g.appendChild(e);
 	} else {
 	  /* Otherwise, we have to do a little more work. */
-	  
 	  float p_x, p_y, q_x, q_y;
-	
 	  /* TODO:
 	   * Eventually this will be a conditional based on user settings. */
 	  if (true) {
@@ -57,14 +58,14 @@ public class Wire {
 		q_y = height;
 	  }
 
-	  e.setAttribute("points", "0,0 " +
-			  p_x + "," + p_y + " " +
-			  q_x + "," + q_y
+	  e.setAttribute("points", "0,0 "
+			  + p_x + "," + p_y + " "
+			  + q_x + "," + q_y
 	  );
 	}
 	g.appendChild(e);
 	svg.appendChild(g);
-	
+
 	/* TODO:
 	 * Overload draw_manager::create_instance with a form that accepts a
 	 * Document. */
