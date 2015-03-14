e945513e3911ff1e16c5c09276aeb63c88875a79
Brent Bessemer <bmb0610@gmail.com>
2015-03-14 14:05:32 -0400

    Moved drag-and-drop stuff to a separate file.
    NOTE: main.js will NOT be run in the browser. It's for desktop-side
    stuff ONLY (file IO, etc.) This is not an ordinary web app. Look up
    Atom shell (http://github.com/atom/atom-shell).

--- src/js/main.js
+++ src/js/main.js
@@ -55,67 +55,10 @@ app.on('ready', function main () {
   // Create the main window and load the UI.
   mainWindow = new BrowserWindow({width: WIDTH, height: HEIGHT});
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  
+
 });
 
-  window.onload = function() {
 
-	var dropZoneOne = document.querySelector('#drop-target-one');
-	var dragElements = document.querySelectorAll('#drag-elements li');
-	var elementDragged = null;
-
-	for (var i = 0; i < dragElements.length; i++) {
-
-		// Event Listener for when the drag interaction starts.
-		dragElements[i].addEventListener('dragstart', function(e) {
-			e.dataTransfer.effectAllowed = 'move';
-			e.dataTransfer.setData('text', this.innerHTML);
-			elementDragged = this;
-		});
-
-		// Event Listener for when the drag interaction finishes.
-		dragElements[i].addEventListener('dragend', function(e) {
-			elementDragged = null;
-		});
-	};
-
-	// Event Listener for when the dragged element is over the drop zone.
-	dropZoneOne.addEventListener('dragover', function(e) {
-		if (e.preventDefault) {
-			e.preventDefault();
-		}
-
-		e.dataTransfer.dropEffect = 'move';
-
-		return false;
-	});
-
-	// Event Listener for when the dragged element enters the drop zone.
-	dropZoneOne.addEventListener('dragenter', function(e) {
-		this.className = "over";
-	});
-
-	// Event Listener for when the dragged element leaves the drop zone.
-	dropZoneOne.addEventListener('dragleave', function(e) {
-		this.className = "";
-	});
-
-	// Event Listener for when the dragged element dropped in the drop zone.
-	dropZoneOne.addEventListener('drop', function(e) {
-		if (e.preventDefault) e.preventDefault(); 
-  	if (e.stopPropagation) e.stopPropagation(); 
-
-		this.className = "";
-		this.innerHTML = "Dropped " + e.dataTransfer.getData('text');
-
-		// Remove the element from the list.
-		document.querySelector('#drag-elements').removeChild(elementDragged);
-		elementDragged = null;
-
-		return false;
-	});
-  });
-  
   // Set the main window to be dereferenced when closed.
   mainWindow.on('closed', function () {
     mainWindow = null;
